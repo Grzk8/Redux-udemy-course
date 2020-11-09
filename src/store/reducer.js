bc7eac853abb146dd3ee7386fb8082e0d6fe0de4
Grzegorz Kaczor <grzesiek125@op.pl>
2020-11-09 18:15:03 +0100

    add reducers

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,25 +1,45 @@
+import * as actionTypes from './actions';
+
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'INCREMENT' :
+        case actionTypes.INCREMENT :
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter +1
+            return  newState;
+            
+        case actionTypes.DECREMENT :
             return {
-                counter: state.counter + 1
-            }
-        case 'DECREMENT' :
-            return {
-                    counter: state.counter - 1
+                ...state, // nowy stan, nie należy manipulować stanem, klonujemy go
+                counter: state.counter - 1
                 }
-        case 'ADD' :
+        case actionTypes.ADD :
             return {
+                ...state,
                 counter: state.counter + action.val
             }
-        case 'SUBTRACT' :
+        case actionTypes.SUBTRACT :
             return {
+                ...state,
                 counter: state.counter - action.val
-            }   
+            } 
+        case actionTypes.STORE_RESULT :
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})   // nie używać push! push zmienia orginalną wartość, contact zwraca nową tablicę - stara + argument
+                 
+            }
+        case actionTypes.DELETE_RESULT :
+             const updatedArray = state.results.filter( result => result.id !== action.resultElId)
+            return {
+                ...state,
+                results: updatedArray
+
+            }  
     }
     return state;
 };
